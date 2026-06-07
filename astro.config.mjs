10655fcf0820b604488494d6b602adb2e1933d9c
hutkuyilmaz <utku@hutkuyilmaz.com>
2026-06-07 17:59:35 +0300

    chore: github pages kurulumu

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,26 @@
 import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  server: {
+    port: 3000,
+  },
+  // 👇 GitHub Pages'teki canlı sitenin tam adresi
   site: 'https://hutkuyilmaz.github.io',
-  base: '/mtx-website',
+
+  // 👇 GitHub'daki repo adın (Başında ve sonunda eğik çizgi olmalı)
+  base: '/mtx-website/',
+
   output: 'static',
   compressHTML: true,
+  trailingSlash: 'ignore',
+
   build: {
     assets: '_astro'
+  },
+
+  vite: {
+    plugins: [tailwindcss()]
   }
 });
