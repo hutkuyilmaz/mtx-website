b643238da0e517a9b4a7e174d2c928033d93e573
hutkuyilmaz <utku@hutkuyilmaz.com>
2026-06-07 12:03:23 +0300

    Feat: Complete single-page landing architecture with secure gh-actions workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,26 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
+/*
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  server: {
+    port: 3000, // Blogun 4321'de çalışırken, Materix burada çalışacak!
+  },
+
+  integrations: [tailwindcss()],
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
+});*/
+
+
+export default defineConfig({
+  // Essential for dynamic sitemaps and structural path normalization
+  site: 'https://hutkuyilmaz.github.io',
+  // Essential for routing assets out of the correct sub-directory container
+  base: 'hutkuyilmaz',
+  integrations: [tailwindcss()],
+});
